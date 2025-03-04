1e66cd572aee723e704c0f5c4cc126966ebb345e
puppalarahulkumar <p.rahulkumarrrs@gmail.com>
2025-03-04 11:19:28 +0530

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig,DataTransformationConfig
 from networksecurity.components.data_validation import DataValidation
 import sys
 
@@ -19,11 +20,18 @@ if __name__=="__main__":
         print(dataingestionartifact)
         data_validation_config=DataValidationConfig(training_pipeline_config)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
-        logging.info("inititated the data validation")
-        data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("initiate the data validation")
-        print(data_validation_artifact)
         
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("initiated the data validation")
+        print(data_validation_artifact)
+        logging.info("data Transformation started")
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        data_transformation=DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation is done")
+
+
     
     except Exception as e:
         raise NetworkSecurityException(e,sys)
